2114026be03c3bb6234139e14a7374d717139c3c
yahia-elakeed <yahia.elakeed@gmail.com>
2018-08-08 22:13:40 -0500

    update models

--- src/main/java/com/mum/asd/OnlineBankingFramework/models/Transfer.java
+++ src/main/java/com/mum/asd/OnlineBankingFramework/models/Transfer.java
@@ -2,4 +2,32 @@ package com.mum.asd.OnlineBankingFramework.models;
 
 public class Transfer {
 
+	private long transferNumber;
+	private Account fromAccount;
+	private Account destinationAccount;
+
+	public long getTransferNumber() {
+		return transferNumber;
+	}
+
+	public void setTransferNumber(long transferNumber) {
+		this.transferNumber = transferNumber;
+	}
+
+	public Account getFromAccount() {
+		return fromAccount;
+	}
+
+	public void setFromAccount(Account fromAccount) {
+		this.fromAccount = fromAccount;
+	}
+
+	public Account getDestinationAccount() {
+		return destinationAccount;
+	}
+
+	public void setDestinationAccount(Account destinationAccount) {
+		this.destinationAccount = destinationAccount;
+	}
+
 }
